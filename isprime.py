f8987ed0f0254c0c16f631cf4489b47a43a3ca96
Matthew Wang <matthewang.wang@gmail.com>
2015-10-09 21:21:15 -0400

    changed way of checking prime to a cooler way
    all primes are of the form 6k ± i, with the exception of 2 and 3. so
    just check if 2/3, see if num is in form 6k+i (except 0,2,3,4,6 as they
    are divisible by 2/3), so check if i = 5 (-1) or if i = 1
    
    lengthy explanation but it works like a charm

--- isprime.py
+++ isprime.py
@@ -5,6 +5,9 @@
 # whether or not a number is prime, not #
 # having 500 error messages. ;), Enjoy! #
 #########################################
+#import
+import math
+
 #function that checks if the input is a number
 def numcheck (num):
 	try:
@@ -12,18 +15,36 @@ def numcheck (num):
 		return True
 	except ValueError:
 		return Falseprint("Prime Number checker loaded.")
+'''
+Boring way to check if number is prime
 #function that checks if a number is prime
 def checkprime (num):
-	for i in range(num):
-		if num % (i+2) == 0 and num != i+2:
+	if (number <= 1):
+		return False
+	for i in range(2,num):
+		if num % (i) == 0 and num != i:
 			return False
 	return True
+'''
+#super cool way to check for prime-ness
+def checkprime (num):
+	if num == 2 or num == 3:
+		return True
+	elif num % 2 == 0 or num % 3 == 0:
+		return False;
+	else:
+		int counter = 5
+		while counter*counter <= num:
+			if num % counter == 0 or num % (counter+2) == 0:
+				return False
+			counter += 6
+		return True
 #main loop for the program
 while True:
-	inp = raw_input("Enter an integer: ")
+	inp = raw_input("Enter an positive integer: ")
 	#checks if input is a number
-	if numcheck(int(inp)) == False:
-		endclause = raw_input("Welp you entered a wrong input! If you want to exit, type in \"esc\". Other inputs will start the program again.")
+	if numcheck(int(inp)) == False or int(inp) < 0:
+		endclause = raw_input("Welp you entered a wrong type input! If you want to exit, type in \"esc\". Other inputs will start the program again.")
 	else:
 		# 0 and 1 are special cases, neither prime nor composite
 		if int(inp) == 0 or int(inp) == 1:
@@ -35,9 +56,9 @@ while True:
 		else:
 			print("Prime")
 		#goes through input check if the user wants to quit
-		endclause = raw_input("Hope that helped! If you want to exit, type in \"esc\". Other inputs will start the program again.")
+		endclause = raw_input("Hope that helped! If you want to continue, type in \"cont\". Other inputs will start the program again.")
 	#breaks mainloop if user wants to leave
-	if endclause == ("esc"):
+	if endclause != ("cont"):
 		break
 	else:
 		print("Restarting ...")
